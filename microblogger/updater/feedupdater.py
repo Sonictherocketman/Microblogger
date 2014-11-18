27a112d9da177f7f6757d64ce8e2c89286549deb
Brian Schrader <sonictherocketman@gmail.com>
2014-11-17 20:57:46 -0800

    added generators for block/follows lists

--- microblogger/updater/feedupdater.py
+++ microblogger/updater/feedupdater.py
@@ -7,20 +7,22 @@ from datetime.datetime import strptime
 import time
 
 from microblogger.post import post
+from microblogger import util as u
 
 def add_post(post):
     """ Adds the given post to the user's feed.  """
     tree = _get_user_feed('user/feed.xml')
     items = tree.XPath('//items')
     items.insert(0, post.to_element(post))
-    _write_user_feed(tree, 'user/feed.xml')
+    u.write_user_feed(tree, 'user/feed.xml')
+
 
 def fetch(start, n=20):
     """ Starting at the starting post id, fetches n posts (assuming the posts are ordered).
     Positive n for posts since start, negative n for previous posts. """
 
     # Get the tree, exract the starting point.
-    tree = _get_user_feed('user/feed.xml')
+    tree = u.get_user_feed('user/feed.xml')
     stati = [post(status) for status in tree.XPath('//item[@guid]')]
 
     starting = stati.index([status for status in stati if status['guid'] == start][0])
@@ -39,29 +41,41 @@ def fetch_top(n=20):
     if n < 0:
         raise IndexError
 
-    tree = _get_user_feed('user/feed.xml')
+    tree = u.get_user_feed('user/feed.xml')
     stati = [post(status) for status in tree.XPath('//item[@guid]')]
     return stati[:n]
 
+
 def delete_post(status_id):
     """ Deletes the post with the given id from the feed.  """
-    tree = _get_user_feed('user/feed.xml')
+    tree = u.get_user_feed('user/feed.xml')
     for bad in tree.XPath('//item[@guid=$status_id]', status_id):
         bad.getparent().remove(bad)
-    _write_user_feed(tree, 'user/feed.xml')
+    u.write_user_feed(tree, 'user/feed.xml')
 
-def _get_user_feed(rel_location):
-    """ Get the etree representation of the feed located at the rel_location.  """
-    tree = None
-    with open(rel_location, 'r') as f:
-        data = f.read()
-        tree = etree.parse(StringIO(data))
-    return tree
 
-def _write_user_feed(tree, rel_location)
-    """ Write the etree representation of the feed to the rel_locaiton.  """
-    with open(rel_location, 'w') as f:
-        tree.write(f)
+def add_blocked_user(user_id, user_link, user_name):
+    """ Adds a given user to the block list.  """
+    # TODO
+
+
+def add_follow_user(user_id, user_link, user_name):
+    """ Adds a given user to the list of users to follow. """
+    # TODO
+
+
+def relocate_user_feed(url):
+    """ Signals to the user's followers to discard the main feed
+    redirect to the given URL. This will cause the user's current
+    feed to be considered dead.
+
+    Use with caution. """
+    feed = u.get_user_feed('user/feed.xml')
+    element = E.relocate(url)
+    channel = feed.XPath('//channel')
+    channel.append(element)
+    u.write_user_feed(feed, 'user/feed.xml')
+
 
 
 
