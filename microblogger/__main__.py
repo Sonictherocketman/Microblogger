b0cc7ca0096aebd26f81082cd3cd48b13dc44ae2
Sonictherocketman <brian@biteofanapple.com>
2014-11-18 16:46:24 -0800

    Added main.py.

---
+++ microblogger/__main__.py
@@ -0,0 +1,9 @@
+""" The launching point for the microblogger service.
+
+Invoking this script should be all that the user needs
+to do to begin using the service.
+
+$ python -m microblogger """
+
+# TODO
+# - Launch web server
