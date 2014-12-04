1c69b4d86239b12d1993e073211e577c62a68b66
Sonictherocketman <brian@biteofanapple.com>
2014-12-04 13:12:16 -0800

    moved feedreader to its new home

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,15 +1,25 @@
 """ A FeedCrawler subclass """
 
 from microblogcrawler.crawler import FeedCrawler
-
+from util import to_cache, add_post_to_cache
+from feedreader import get_user_follows, get_user_blocks
 
 class MicroblogFeedCrawler(FeedCrawler):
     """ A subclass of the microblogcrawler. """
 
+    def __init__(self):
+        """ Init a new crawler. """
+        # If a user is blocking a given user, then do
+        # not show their messages or replies.
+        self.block_list = []
+        self.ignore_user = False
+
+        super(MicroblogFeedCrawler, self).__init__()
+
     def on_start(self):
-        """ Handle new feeds being inserted. """
-        # TODO
-        pass
+        """ Refresh the follows list and the blocks list. """
+        self.block_list = get_user_blocks()
+        return get_user_follows()
 
     def on_finish(self):
         """ Handle end of crawling process. """
